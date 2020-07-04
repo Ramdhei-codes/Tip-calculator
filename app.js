b53dec15974b6b956e0e2229190295848fc3487a
Ramdhei <ramdhei2002@gmail.com>
2020-07-04 16:13:48 -0500

    Added error message with SweetAlert

---
+++ app.js
@@ -0,0 +1,62 @@
+const billAmount = document.getElementById('billAmount')
+const serviceQuality = document.getElementById('serviceQuality')
+const peopleSharing = document.getElementById('peopleSharing')
+const button = document.getElementById('button')
+
+const serviceToNumber = Quality => {
+    switch (Quality.value) {
+        case "Excellent":
+            return 0.15
+        case "Good":
+            return 0.1
+        case "Regular":
+            return 0.07
+        case "Bad":
+            return 0
+    }
+}
+
+const verifyQuality = () => {
+    if (serviceQuality.value === "Choose an option") {
+        swal({
+            title: 'Error',
+            icon: 'error',
+            text: 'Please choose the quality of the service'
+        })
+        
+    }
+}
+
+function calculate(){
+    if (serviceQuality.value === "Choose an option" || billAmount.value === "" || peopleSharing.value === "") {
+        showWarning()
+    } else {
+        let bill = Number(billAmount.value)
+        let people = Number(peopleSharing.value)
+        let service = serviceToNumber(serviceQuality)
+    
+        let tipPerPerson = ((bill * service)/people).toFixed(2)
+        let billPerPerson = ((bill + (bill * service))/people).toFixed(2)
+        
+        showResults(tipPerPerson, billPerPerson)
+    }
+}
+
+const showResults = (tipPP, billPP) => {
+    swal({
+        title: 'Bill and tip per person: ',
+        text: `Bill: $${billPP}
+                Tip: $${tipPP}`,
+
+        //title: 'Tip per person: ',
+        //text: `$${tipPP}`
+    })
+}
+
+const showWarning = () => {
+    swal({
+        title: 'Error',
+        icon: 'error',
+        text: 'Incomplete data'
+    })
+}
